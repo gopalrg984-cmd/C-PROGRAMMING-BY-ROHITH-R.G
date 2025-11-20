d450a2fc61da1e1d20a761d588860230c76676ee
gopalrg984-cmd <gopalrg984@gmail.com>
2025-11-20 10:52:18 +0530

    Create strlength.c

---
+++ strlength.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<string.h>
+    int main(){
+        char name[10];
+        printf("enter the name\n");
+        scanf("%s",name);
+        int length=strlen(name);
+        printf("the length is: %d\n",length);
+    }
+ 
